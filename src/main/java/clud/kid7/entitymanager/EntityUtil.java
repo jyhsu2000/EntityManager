98dbad15635abb39996834f5e483e78977a6338d
KID <jyhsu2000@yahoo.com.tw>
2019-02-24 23:00:28 +0800

    Change limitation to LivingEntity except for Monster

--- src/main/java/clud/kid7/entitymanager/EntityUtil.java
+++ src/main/java/clud/kid7/entitymanager/EntityUtil.java
@@ -1,8 +1,9 @@
 package clud.kid7.entitymanager;
 
 import org.bukkit.Chunk;
-import org.bukkit.entity.Animals;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Monster;
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,14 +16,12 @@ public class EntityUtil {
         if (chunk == null) {
             return true;
         }
-        //只處理動物
-        if (!(entity instanceof Animals)) {
-            return true;
-        }
         //該區塊所有實體
         Entity[] entities = chunk.getEntities();
-        //該區塊所有動物
-        List<Entity> livingEntities = Arrays.stream(entities).filter(entity1 -> entity1 instanceof Animals)
+        //該區塊所有非怪物生物
+        List<Entity> livingEntities = Arrays.stream(entities)
+            .filter(entity1 -> entity1 instanceof LivingEntity)
+            .filter(entity1 -> !(entity1 instanceof Monster))
             .collect(Collectors.toList());
         //計算生物數量
         long entityCount = livingEntities.size();
