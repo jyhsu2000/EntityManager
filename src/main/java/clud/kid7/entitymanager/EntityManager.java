88dd898345be9c5043a2ca878dddd00e1bcc9bea
KID <jyhsu2000@yahoo.com.tw>
2019-02-24 20:40:06 +0800

    Create project

---
+++ src/main/java/clud/kid7/entitymanager/EntityManager.java
@@ -0,0 +1,19 @@
+package clud.kid7.entitymanager;
+
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EntityManager extends JavaPlugin {
+    public static final String PREFIX = ChatColor.translateAlternateColorCodes('&', "&6[&a實體管理&6] &r");
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
