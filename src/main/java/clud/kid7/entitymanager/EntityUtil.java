1a1703f4fe27e28ef973617f28ecfc64d5c588f0
KID <jyhsu2000@yahoo.com.tw>
2019-02-24 22:18:21 +0800

    Set limit for only animals

--- src/main/java/clud/kid7/entitymanager/EntityUtil.java
+++ src/main/java/clud/kid7/entitymanager/EntityUtil.java
@@ -1,8 +1,8 @@
 package clud.kid7.entitymanager;
 
 import org.bukkit.Chunk;
+import org.bukkit.entity.Animals;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,10 +15,14 @@ public class EntityUtil {
         if (chunk == null) {
             return true;
         }
+        //只處理動物
+        if (!(entity instanceof Animals)) {
+            return true;
+        }
         //該區塊所有實體
         Entity[] entities = chunk.getEntities();
-        //該區塊所有生物
-        List<Entity> livingEntities = Arrays.stream(entities).filter(entity1 -> entity1 instanceof LivingEntity)
+        //該區塊所有動物
+        List<Entity> livingEntities = Arrays.stream(entities).filter(entity1 -> entity1 instanceof Animals)
             .collect(Collectors.toList());
         //計算生物數量
         long entityCount = livingEntities.size();
