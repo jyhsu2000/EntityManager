eee478d816738b5333bc035924cb7dfbb95ea99b
KID <jyhsu2000@yahoo.com.tw>
2019-09-30 18:19:49 +0800

    Auto change particle count to 1 if it's BARRIER

--- src/main/java/clud/kid7/entitymanager/ParticleUtil.java
+++ src/main/java/clud/kid7/entitymanager/ParticleUtil.java
@@ -6,6 +6,7 @@ import org.bukkit.Particle;
 public class ParticleUtil {
     public static void playEffect(Location location, Particle particle) {
         Location effectLocation = location.clone().add(0, 1.2, 0);
-        location.getWorld().spawnParticle(particle, effectLocation, 10, 0.2, 0.2, 0.2, 0);
+        int particleCount = particle.equals(Particle.BARRIER) ? 1 : 10;
+        location.getWorld().spawnParticle(particle, effectLocation, particleCount, 0.2, 0.2, 0.2, 0);
     }
 }
