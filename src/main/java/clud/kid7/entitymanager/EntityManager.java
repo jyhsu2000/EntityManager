5dd89b73a6751907e1745953006d3ed0a79125e4
KID <jyhsu2000@yahoo.com.tw>
2019-09-30 18:10:55 +0800

    Remove unused PREFIX variable

--- src/main/java/clud/kid7/entitymanager/EntityManager.java
+++ src/main/java/clud/kid7/entitymanager/EntityManager.java
@@ -1,11 +1,8 @@
 package clud.kid7.entitymanager;
 
-import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EntityManager extends JavaPlugin {
-    public static final String PREFIX = ChatColor.translateAlternateColorCodes('&', "&6[&a實體管理&6] &r");
-
     @Override
     public void onEnable() {
         // Plugin startup logic
